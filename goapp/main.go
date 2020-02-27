53fd6f0b73675661d623c71d8e70a1cebce4d5c1
Le Anh Duc <ducla@lifull-tech.vn>
2020-02-27 17:45:45 +0700

    Update main.go

--- goapp/main.go
+++ goapp/main.go
@@ -1,33 +1,31 @@
 package main
 
 import (
-    "net/http"  
+	"net/http"
 
-    "github.com/go-playground/validator/v10"  
-    "github.com/julienschmidt/httprouter"  
+	"github.com/go-playground/validator/v10"
+	"github.com/julienschmidt/httprouter"
 
-   "api/config"
-   "api/infrastructure/api/router"
-   "api/infrastructure/datastore"
-   "api/registry"
+	"api/config"
+	"api/infrastructure/api/router"
+	"api/infrastructure/datastore"
+	"api/registry"
 )
 
-func main() { 
-    config.LoadConfig()  
+func main() {
+	config.LoadConfig()
 
-    db := datastore.NewMySQL()  
-    ps := datastore.NewPostgreSQL()  
-    r := httprouter.New()  
-    v := validator.New()  
+	db := datastore.NewMySQL()
+	ps := datastore.NewPostgreSQL()
+	r := httprouter.New()
+	v := validator.New()
 
-    rg := registry.NewInteractor(db, ps, v)               
-    h := rg.NewAppHandler()                              
+	rg := registry.NewInteractor(db, ps, v)
+	h := rg.NewAppHandler()
 
-    router.NewRouter(r, h)   
-                    
-    defer db.Close()                     
-                      
-    http.ListenAndServe(":8080", r)  
-}     
+	router.NewRouter(r, h)
 
-// asdasddas asdas 
+	defer db.Close()
+
+	http.ListenAndServe(":8080", r)   
+}
