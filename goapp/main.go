a37806c8040ff54b191dfc0231abf84794cab665
Le Anh Duc <ducla@lifull-tech.vn>
2020-02-27 17:41:55 +0700

    Update main.go

--- goapp/main.go
+++ goapp/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "net/http"
+    "net/http"  
 
-    "github.com/go-playground/validator/v10"
-    "github.com/julienschmidt/httprouter"
+    "github.com/go-playground/validator/v10"  
+    "github.com/julienschmidt/httprouter"  
 
    "api/config"
    "api/infrastructure/api/router"
@@ -12,13 +12,13 @@ import (
    "api/registry"
 )
 
-func main() {
-    config.LoadConfig()
+func main() { 
+    config.LoadConfig()  
 
-    db := datastore.NewMySQL()
-    ps := datastore.NewPostgreSQL()
-    r := httprouter.New()
-    v := validator.New()
+    db := datastore.NewMySQL()  
+    ps := datastore.NewPostgreSQL()  
+    r := httprouter.New()  
+    v := validator.New()  
 
     rg := registry.NewInteractor(db, ps, v)               
     h := rg.NewAppHandler()                              
